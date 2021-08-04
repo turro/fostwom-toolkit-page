65d9d8b62b03f6d4094a3d9d350e97b253f9f310
Marcos H <36385884+mhaba@users.noreply.github.com>
2021-08-04 14:57:48 +0200

    First commit

---
+++ src/js/questions.js
@@ -0,0 +1,40 @@
+const questions = [
+    {
+        question: "Are female content experts part of the teaching staff of this MOOC?",
+        answer: "If you have a chance to involve women as role models, testimonials, case study representatives, please do so."
+    },
+    {
+        question: "Do you reflect on yourself and your professional role in a gender conscious way?",
+        answer: "Be open to reflect on yourself and your professional role in a gender conscious way.\
+        \
+        We suggest going through the list together with the other teachers or experts involved in designing the MOOC in order to begin thinking and reflecting \
+        on how these issues influence teaching and learning in your specific subject. (It's about learning new skills, it's not enough going through the checklist).\
+        \
+        Gender is an area that most people have opinions about, but fewer have solid knowledge. It is also in many cases about things that affect personal values, \
+        which force you to reflect on yourself and your identity. Becoming gender aware is often a slow process of knowledge and learning. Time is needed for \
+        dialogue and reflection based on the scientific knowledge that is available. Without an in-depth understanding of how gender is done in everyday practices, \
+        it is also close at hand that gender-blind and homosocial practices continue without being problematized."
+    },
+    {
+        question: "Do you reflect on how gender influences teaching and learning in your specific subject?",
+        answer: "We suggest going through the checklist together with other teachers or experts involved in designing the MOOC in order to begin \
+        thinking and reflecting on how gender influences teaching and learning in your specific subject. (It's about learning new skills, it's not enough going through \
+        the checklist without reflection)."
+    },
+    {
+        question: "Do you actively seek ways to be inclusive to both women and men in your approach to the design of this MOOC?",
+        answer: ""
+    },
+    {
+        question: "",
+        answer: ""
+    },
+    {
+        question: "",
+        answer: ""
+    },
+    {
+        question: "",
+        answer: ""
+    },
+];
